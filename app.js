83fc35a85f33d69cc09cb09352a7e8013d66e990
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-26 15:39:22 -0400

    Operadores artiméticos

---
+++ app.js
@@ -0,0 +1,45 @@
+"use strict"
+
+/* Operadores Aritméticos */
+
+var a = 10;
+var b = 20;
+
+/* Suma */
+
+var suma = a + b;
+console.log("La suma de " + a + " + " + b + " es igual a: " + suma);
+
+/* Resta */
+
+var resta = a - b;
+console.log("La resta de " + a + " - " + b + " es igual a: " + resta);
+
+/* Multiplicación */
+
+var mult = a * b;
+console.log("La multiplicación de " + a + " x " + b + " es igual a: " + mult);
+
+/* División */
+
+var div = a / b;
+console.log("La división entre " + a + " / " + b + " es igual a: " + div);
+
+/* Modulo o Residuo */
+
+var mod = a % b;
+console.log("El módulo o residuo de " + a + " % " + b + " es igual a: " + mod);
+
+/* Incremento */
+
+var inc = a;
+inc++;
+console.log("El incremento de " + a + " es igual a: " + inc);
+
+/* Decremento */
+
+var der = a;
+der--;
+console.log("El decremento de " + a + " es igual a: " + der);
+ 
+
