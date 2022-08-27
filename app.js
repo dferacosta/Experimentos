9acc9901daecba08f70904f767a68f55030b60a4
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-27 19:11:36 -0400

    Operador ternario o condicional

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 "use strict"
 
-/* OPERACIONES CON CONCATENACION */
+/* OPERADOR TERNARIO O CONDICIONAL */
 
-var a = 10;
+var a = 110;
 var b = 20;
 
-var nombre = "David";
-var apellidos = "Ferreira Acosta";
-
-/* CONCATENACION DE NUMEROS */
-
-var concnum = a + b
-console.log("La suma de " + a + " y " + b + " es igual a: " + concnum);
-
-/* CONCATENACION DE CADENAS DE TEXTO */
-
-var conctext = (nombre + " " + apellidos);
-console.log("El Full Stack Developer del año es: " + conctext);
-
-/* CONCATENACION DE NUMEROS COMO TEXTO */
-
-var cnumtxt = "19"+"96";
-console.log("El año de naciento de " + nombre + " " + apellidos + " es " + cnumtxt);
-
-
-/* CONCATENACION DE TEXTO Y NUMERO */
-
-var ctextnumb = a + "b";
-console.log("La union de un valor numérico y texto se visualiza de la siguinte manera: " + ctextnumb);
-
-
+/* CONDICION "?" TRUE : FALSE */
+var resultado = (a > b ? "Si, es mayor." : "No, no es mayor.");
+console.log("El resultado con el operador ternario u operador condicional es: " + resultado);
