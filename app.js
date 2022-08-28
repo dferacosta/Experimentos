10636bf631f6f41f3032cc0550f74ac13013e8b4
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-27 22:14:53 -0400

    Condición IF ELSE

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var a = 10;
 var b = 20;
 var resultado = "Sin datos";
 
-if ( a > b);{
+if ( a > b){
     resultado = "La condición se cumplió";
+} else {
+    resultado = "La condición no se cumplió"
 }
 
 console.log("Cuál es el resultado final de IF? " + resultado);
