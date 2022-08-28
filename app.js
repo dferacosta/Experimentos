e18c7f0590418dee6c06df48c9bdf91727899428
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-28 12:12:09 -0400

    Ciclos en JavaScript: FOR

--- app.js
+++ app.js
@@ -1,57 +1,11 @@
 "use strict"
 
-/* ESTRUCTURA SWITCH */
+// CICLO FOR | CICLO DEFINIDO
+// REPETICION = ITERACION
 
-//EVALUACION CON NUMEROS
 
-var edad = 30;
-var resultado = "";
+var productos = 5;
 
-switch (edad) {
-    case 10:
-            resultado = ("La edad es 10 años");        
-        break;
-
-    case 20:
-            resultado = ("La edad es 20 años");
-        break;
-
-    case 30:
-            resultado = ("La edad es 30 años");
-        break;
-
-    case 40:
-            resultado = ("La edad es 40 años");
-        break;
-
-    case 50:
-            resultado = ("La edad es 50 años");
-        break;
-
-    default:
-            resultado("Ningún caso coincide");
-        break;
+for (let contador = 0; contador < productos; contador++){
+    console.log("Producto #" + contador);
 }
-console.log("El resultado de la evaluación con números es: " + resultado);
-
-/* EVALUACION CON CADENAS DE TEXTO */
-
-var producto = "TV";
-
-switch (producto) {
-    case "TV":
-            resultado = "Se eligió TV";        
-        break;
-    case "Radio":
-            resultado = "Se eligió Radio";
-        break;
-    case "Teléfono":
-            resultado = "Se eligió el teléfono";
-        break;
-    default:
-            resultado = "No se eligió  ningún producto";
-        break;
-}
-
-console.log("El resultado de la evaluación de texto es: " + resultado);
-
