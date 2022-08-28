ff3217ed9bacad3d1df9b353db4aa1925fdb5a7f
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-28 10:44:39 -0400

    Uso de Switch

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 "use strict"
 
-/* CONDICION IF */
+/* ESTRUCTURA SWITCH */
 
-var a = 10;
-var b = 20;
-var resultado = "Sin datos";
+//EVALUACION CON NUMEROS
 
-if ( a > b){
-    resultado = "La condición se cumplió";
-} else {
-    resultado = "La condición no se cumplió"
+var edad = 30;
+var resultado = "";
+
+switch (edad) {
+    case 10:
+            resultado = ("La edad es 10 años");        
+        break;
+
+    case 20:
+            resultado = ("La edad es 20 años");
+        break;
+
+    case 30:
+            resultado = ("La edad es 30 años");
+        break;
+
+    case 40:
+            resultado = ("La edad es 40 años");
+        break;
+
+    case 50:
+            resultado = ("La edad es 50 años");
+        break;
+
+    default:
+            resultado("Ningún caso coincide");
+        break;
+}
+console.log("El resultado de la evaluación con números es: " + resultado);
+
+/* EVALUACION CON CADENAS DE TEXTO */
+
+var producto = "TV";
+
+switch (producto) {
+    case "TV":
+            resultado = "Se eligió TV";        
+        break;
+    case "Radio":
+            resultado = "Se eligió Radio";
+        break;
+    case "Teléfono":
+            resultado = "Se eligió el teléfono";
+        break;
+    default:
+            resultado = "No se eligió  ningún producto";
+        break;
 }
 
-console.log("Cuál es el resultado final de IF? " + resultado);
+console.log("El resultado de la evaluación de texto es: " + resultado);
+
