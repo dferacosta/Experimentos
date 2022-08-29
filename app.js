18438a400498f8146dd4af3182b4d1f96d8fe9c7
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-29 08:40:30 -0400

    Operador: THIS

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 "use strict"
-/* FUNCIONES ARROW | FAT ARROW |  LAMBDA FUNCTIONS */
 
-//PRIMER EJEMPLO
-var saludar = nombre => "Hola, " + nombre;
-console.log(saludar("David."));
+const boton = document.querySelector(".boton");
 
-//SEGUNDO EJEMPLO
-var sumar = cantidad => cantidad + 10;
-console.log(sumar(10));
-
-//TERCER EJEMPLO
-var calcular = (datoA, datoB) => datoA + datoB
-console.log(calcular(25,75));
-
-//CUARTO EJEMPLO
-var generar = (datoA, datoB) => {
-    var datoC = 45;
-    return (datoA + datoB + datoC);
-}
-console.log(generar((25*5),75));
-
-//QUINTO EJEMPLO
-var validar = () => {
-    return "Validación correcta";
-}
-console.log(validar());
+boton.addEventListener("click", () => {
+    console.log(this)
+    this.location = "https://www.google.com";
+})
