d3651af771def5f68b685699366cda2a4a09ccf6
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-08-30 16:54:32 -0400

    Métodos de búsqueda

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 "use strict"
 
-/* MEDIR CADENAS DE TEXTO */
+/* METODOS DE BUSQUEDA | PART 1*/
 
-var mensaje = 'Estoy aprendiendo JavaScript';
-//Los espacios tambien son contados como digitos dentro de una cadena de texto.
+var mensaje = "Estoy aprendiendo JavaScript y estoy aprendiendo mucho";
 
-console.log('La cadena de texto tiene: ' + mensaje.length + ' letras');
+var resultado = "";
+
+console.log("🔸" + "Palabra buscada: 'Aprendiendo" + " , oración: " + mensaje);
+console.log(" ");
+
+//indexOf
+console.log("indexOf")
+resultado = "La palabra buscada ocupa el puesto no. " + mensaje.indexOf("aprendiendo");
+console.log(resultado);
+console.log(" ");
+
+//lastIndexOf
+console.log("lastIndexOf")
+resultado = "La palabra buscada ocupa el puesto no. " + mensaje.lastIndexOf('aprendiendo');
+console.log(resultado);
+console.log(" ");
+
+//search:
+console.log("search");
+resultado = "La palabra buscada ocupa el puesto no. " + mensaje.search('aprendiendo');
+console.log(resultado);
+console.log(" ");
+
+
+//search | Exresion regular
+console.log("search: 'i'");
+resultado = "La palabra buscada ocupa el puesto no. " + mensaje.search(/ja/i);
+//i: "In case Sensitive". Cuando el resultado es igual a -1 quiere decir que no enctró una coincidencia.    
+console.log(resultado);
+console.log(" ");
 
 
