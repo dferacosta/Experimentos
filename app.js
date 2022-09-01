45cf9ec59808a4c870d0951df16a3f8728284352
David Ferreira A <109878644+dferacosta@users.noreply.github.com>
2022-09-01 18:18:52 -0400

    Ordenando arreglos (Arrays)

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 "use strict"
-/* GENERACION DE ARREGLOS CON "SPLIT", "FROM", Y "OF" */
 
-/* SPLIT */
-console.log("GENERACION DE ARREGLOS CON 'SPLIT'");
-var mensaje = "ceviche, tacos, pasta";
-var platillos = mensaje.split(",");
+/* ORDENANDO ARREGLOS */
 
-console.log(platillos);
-console.log(" ");
+var platillos = ["Ceviche", "Tacos", "Pasta"];
+console.log("Antes: ", platillos);
 
-/* FROM */
-console.log("GENERACION DE ARREGLOS CON 'FROM'");
-var plat1 = Array.from(document.querySelectorAll(".platillos p"));
-var plat2 = plat1.map(platillo => platillo.textContent);
-                                    
-console.log(plat2);
-console.log(" ");
 
-/* OF */
-console.log("GENERACION DE ARREGLOS CON 'OF'");
-var plat3 = Array.of("Ceviche", "Tacos", "Pasta");
 
-console.log(plat3);
-console.log(" ");
+platillos.sort();
+console.log("Después: ", platillos);
+
+platillos.reverse();
+console.log("Uso de 'Reverse': ", platillos);
